15f46424de2e6c6804645c1f122bba18139d0487
Amber Morris <ambermorris1997@gmail.comgit config --global core.editor vimgit config --global core.autocrlf inputgit config --global user.email ambermorris1997@gmail.com>
2023-09-08 01:37:41 -0700

    fixing build filepath

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from 'react-router-dom';
 
-import '/public/styles/main.css';
+import '../public/styles/main.css';
 
 ReactDOM.render( <App />, document.getElementById('app'));

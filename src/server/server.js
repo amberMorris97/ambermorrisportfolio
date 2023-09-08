9987ded081adcebcb14248cfb0d7ad51b5717741
Amber Morris <ambermorris1997@gmail.comgit config --global core.editor vimgit config --global core.autocrlf inputgit config --global user.email ambermorris1997@gmail.com>
2023-09-07 21:06:18 -0700

    testing filepath for deploy bug

--- src/server/server.js
+++ src/server/server.js
@@ -6,6 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../../public')));
 
 module.exports = app;
